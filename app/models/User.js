02863dd39283ea158cc07f1b0b72afd50e77498a
Ian Erickson <Ian@micrognomics.com>
2022-06-16 11:57:16 -0400

    Update bio and user to be more customized.

--- app/models/User.js
+++ app/models/User.js
@@ -1,5 +1,7 @@
 'use strict';
+//Tells Javascript to be superstrict withe code here. No syntax errors allowed.
 const mongoose = require( 'mongoose' );
+//A mongoose object was already created in app.js, so it automatically uses that object
 const Schema = mongoose.Schema;
 
 var userSchema = Schema( {
@@ -9,3 +11,5 @@ var userSchema = Schema( {
 } );
 
 module.exports = mongoose.model( 'User', userSchema );
+//This creates a mongoose model with the name 'User'. 
+//By convention mongoose models are usually singular and capitalized.
